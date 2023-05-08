3bc43c6d0bb8a73f6988948ae24b18f3a8903a65
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-07 22:59:48 -0600

    started, will need more xp before i can finish this

--- lib/models.py
+++ lib/models.py
@@ -2,17 +2,14 @@ from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
-
-Base = declarative_base(metadata=metadata)
+convention = {"fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",}
+metadata = MetaData(naming_convention = convention)
+Base = declarative_base(metadata = metadata)
 
 class Company(Base):
     __tablename__ = 'companies'
 
-    id = Column(Integer(), primary_key=True)
+    id = Column(Integer(), primary_key = True)
     name = Column(String())
     founding_year = Column(Integer())
 
@@ -22,8 +19,18 @@ class Company(Base):
 class Dev(Base):
     __tablename__ = 'devs'
 
-    id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    id = Column(Integer(), primary_key = True)
+    name = Column(String())
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key = True)
+    item_name = Column(String())
+    value = Column(Integer())
+    dev_id = Column(Integer)
+    company_id = Column(Integer)
+
